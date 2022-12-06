0e36f77a72c76928508c577de31bd59f287f3031
Vamsee Achanta <vamsee.achanta@aceengineer.com>
2022-12-05 19:31:34 -0600

    first issue

---
+++ src/energydata/__main__.py
@@ -0,0 +1,21 @@
+"""Energy Data Library
+
+Usage:
+------
+
+
+
+Contact:
+--------
+
+More information is available at:
+
+- https://pypi.org/project/energydata/
+- https://github.com/vamseeachanta/energydata
+
+
+Version:
+--------
+
+- energydata v0.0.1
+"""
---
+++ src/energydata/tests/test_production.py
@@ -0,0 +1,7 @@
+
+from energydata.common.data_models_components import DataModelsComponents
+
+cfg = docs\code_superseded\bsee_yearly_production_data.yml
+
+dm = DataModelsComponents()
+
