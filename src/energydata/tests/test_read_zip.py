18d2d312ba53e9e08b22c59118018fc3d06b2e99
samdansk2 <samdanshaik8998@gmail.com>
2024-10-09 11:11:19 +0530

    add test for zip

---
+++ src/energydata/tests/test_read_zip.py
@@ -0,0 +1,19 @@
+import zipfile
+
+def test_read_zip_file():
+    zip_path = r"data\bsee\APIRawData.zip"
+    try:
+        with zipfile.ZipFile(zip_path, 'r') as zip_file:
+            # List files the Zip 
+            file_names = [f for f in zip_file.namelist() if not f.endswith('/')]
+            print("Files in the Zip archive:", file_names)
+            
+            for file_name in file_names:
+                if not file_name.endswith('/'):
+                    with zip_file.open(file_name) as file:
+                        data = file.read()
+
+    except FileNotFoundError:
+        print("Error: Zip file path not found.")
+
+test_read_zip_file()
