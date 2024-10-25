30816d3d93ef585003925f2e0ea5da5fc29d0bb1
samdansk2 <samdanshaik8998@gmail.com>
2024-10-25 12:02:52 +0530

    scrapy

--- src/energydata/custom/bsee_data_api_scrapy.py
+++ src/energydata/custom/bsee_data_api_scrapy.py
@@ -1,5 +1,5 @@
 import scrapy
-from scrapy.utils.response import open_in_browser
+from scrapy.utils.response import open_in_browser # useful while program is running
 from scrapy.crawler import CrawlerProcess
 from scrapy import FormRequest
 import pandas as pd
@@ -7,12 +7,10 @@ import pandas as pd
 class BSEEDataSpider(scrapy.Spider):
 
     name = 'API_well_data'
-    start_urls = ['https://www.data.boem.gov/Well/Borehole/Default.aspx']
+    start_urls = ['https://www.data.bsee.gov/Well/APD/Default.aspx']
 
     def parse(self, response):
         data = {
-            '__EVENTTARGET': '',
-            '__EVENTARGUMENT': '',
             'ASPxFormLayout1$ASPxTextBoxAPI': '608164024500',
             'ASPxFormLayout1$ASPxButtonSubmitQ': 'Submit Query'
         }
@@ -21,13 +19,15 @@ class BSEEDataSpider(scrapy.Spider):
     def step2(self, response):
 
         data = {
+            'ASPxFormLayout1$ASPxTextBoxAPI': '608164024500',
             '__EVENTTARGET': 'ASPxFormLayout2$btnCsvExport',
-            '__EVENTARGUMENT': 'Click',
+            '__EVENTARGUMENT': 'Click'
         }
         yield FormRequest.from_response(response,formdata=data, callback=self.parse_csv_data)
 
     def parse_csv_data(self, response): 
-        pass
+        with open('downloaded_data.csv', 'wb') as f:
+            f.write(response.body)
 
 def run_spider():
     process = CrawlerProcess()
