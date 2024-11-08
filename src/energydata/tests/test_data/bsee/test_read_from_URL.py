dd8cb90cd95f8867dd120d23286cf4cba569b516
samdansk2 <samdanshaik8998@gmail.com>
2024-11-08 12:04:19 +0530

    improve zipurl

--- src/energydata/tests/test_data/bsee/test_read_from_URL.py
+++ src/energydata/tests/test_data/bsee/test_read_from_URL.py
@@ -3,22 +3,22 @@ import zipfile
 import io
 import pandas as pd
 import os
-#from urllib.parse import urlparse
+from urllib.parse import urlparse
 
 def download_and_process_zip(url, output_dir):
     # Extract the name from the URL 
-    # base_name_csv = os.path.basename(urlparse(url).path).replace('.zip', '')
+    base_name = os.path.basename(urlparse(url).path).replace('.zip', '')
 
-    r = requests.get(url)
-    r.raise_for_status()  # Check if the download was successful
+    response = requests.get(url)
+    response.raise_for_status()  # Check if the download was successful
 
-    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z = zipfile.ZipFile(io.BytesIO(response.content))
     extracted_files = z.namelist()
 
     for file in extracted_files:
         if file.endswith('/'):
             continue
-        csv_filename = os.path.splitext(os.path.basename(file))[0]+'.csv'
+        csv_name = f"{base_name}_{os.path.splitext(os.path.basename(file))[0]}"+'.csv'
         with z.open(file) as file:   
             try:
                 df = pd.read_csv(file, sep=',', encoding='ISO-8859-1', low_memory=False, nrows=100)
@@ -26,7 +26,7 @@ def download_and_process_zip(url, output_dir):
                 print(f"Could not read {file} as CSV: {e}")
                 continue
 
-            df.to_csv(os.path.join(output_dir, csv_filename), index=False)
+            df.to_csv(os.path.join(output_dir, csv_name), index=False)
 
 
 urls = [
@@ -34,7 +34,7 @@ urls = [
     'https://www.data.bsee.gov/Well/Files/BHPSRawData.zip',
     'https://www.data.bsee.gov/Well/Files/eWellAPDRawData.zip',
     'https://www.data.bsee.gov/Well/Files/eWellAPMRawData.zip',
-    'https://www.data.bsee.gov/Well/Files/eWellEORRawData.zip',
+    # 'https://www.data.bsee.gov/Well/Files/eWellEORRawData.zip',
     'https://www.data.bsee.gov/Well/Files/eWellWARRawData.zip',
     'https://www.data.bsee.gov/Production/Files/ProductionRawData.zip',
     'https://www.data.bsee.gov/Well/Files/APDRawData.zip',
@@ -67,23 +67,23 @@ urls = [
     'https://www.data.bsee.gov/Pipeline/Files/RowDescRawData.zip',
     'https://www.data.bsee.gov/Other/Files/ScannedDocsRawData.zip',
     'https://www.data.bsee.gov/Leasing/Files/SerialRegRawData.zip'
-    'https://factpages.sodir.no/downloads/csv/afxAreaCurrent.zip',
-    'https://factpages.sodir.no/downloads/csv/afxAreaSplitByBlock.zip',
-    'https://factpages.sodir.no/downloads/csv/prlAreaCurrent.zip',
-    'https://factpages.sodir.no/downloads/csv/prlAreaSplitByBlock.zip',
-    'https://factpages.sodir.no/downloads/csv/apaAreaGross.zip',
-    'https://factpages.sodir.no/downloads/csv/apaAreaNet.zip',
-    'https://factpages.sodir.no/downloads/csv/wlbPoint.zip',
-    'https://factpages.sodir.no/downloads/csv/baaAreaCurrent.zip',
-    'https://factpages.sodir.no/downloads/csv/baaAreaSplitByBlock.zip',
-    'https://factpages.sodir.no/downloads/csv/fldArea.zip',
-    'https://factpages.sodir.no/downloads/csv/dscArea.zip',
-    'https://factpages.sodir.no/downloads/csv/fclPoint.zip',
-    'https://factpages.sodir.no/downloads/csv/seaArea.zip',
-    'https://factpages.sodir.no/downloads/csv/pipLine.zip',
-    'https://factpages.sodir.no/downloads/csv/blkArea.zip',
-    'https://factpages.sodir.no/downloads/csv/qadArea.zip',
-    'https://factpages.sodir.no/downloads/csv/subArea.zip'
+    # 'https://factpages.sodir.no/downloads/csv/afxAreaCurrent.zip',
+    # 'https://factpages.sodir.no/downloads/csv/afxAreaSplitByBlock.zip',
+    # 'https://factpages.sodir.no/downloads/csv/prlAreaCurrent.zip',
+    # 'https://factpages.sodir.no/downloads/csv/prlAreaSplitByBlock.zip',
+    # 'https://factpages.sodir.no/downloads/csv/apaAreaGross.zip',
+    # 'https://factpages.sodir.no/downloads/csv/apaAreaNet.zip',
+    # 'https://factpages.sodir.no/downloads/csv/wlbPoint.zip',
+    # 'https://factpages.sodir.no/downloads/csv/baaAreaCurrent.zip',
+    # 'https://factpages.sodir.no/downloads/csv/baaAreaSplitByBlock.zip',
+    # 'https://factpages.sodir.no/downloads/csv/fldArea.zip',
+    # 'https://factpages.sodir.no/downloads/csv/dscArea.zip',
+    # 'https://factpages.sodir.no/downloads/csv/fclPoint.zip',
+    # 'https://factpages.sodir.no/downloads/csv/seaArea.zip',
+    # 'https://factpages.sodir.no/downloads/csv/pipLine.zip',
+    # 'https://factpages.sodir.no/downloads/csv/blkArea.zip',
+    # 'https://factpages.sodir.no/downloads/csv/qadArea.zip',
+    # 'https://factpages.sodir.no/downloads/csv/subArea.zip'
 
 ]
 output_dir = r'src\energydata\tests\test_data\bsee\results\Data\by_zip'
