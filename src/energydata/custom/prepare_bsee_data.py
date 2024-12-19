8338d0201ce5c9b574de340ca65221fa05c4704e
samdansk2 <samdanshaik8998@gmail.com>
2024-12-19 11:19:29 +0530

    add test

--- src/energydata/custom/prepare_bsee_data.py
+++ src/energydata/custom/prepare_bsee_data.py
@@ -1,35 +1,45 @@
 import os
 import pandas as pd
-import yaml
 
 class PrepareBseeData:
 
     def router(self, cfg):
 
-        data = self.merge_columns_from_files(cfg)
+        output_dfs = {}
+        categories = cfg.get("categories", [])
 
-        return data
+        for category in categories:
+            label = category["label"]
+            files = category["files"]
+            
+            output_dfs[label] = self.merge_columns_from_files(cfg, files, label)
+        
+        return output_dfs
     
-    def merge_columns_from_files(self, cfg):
-  
-        #config = load_yaml_config(config_path)
+    def merge_columns_from_files(self, cfg, files, label):
+        
+        data_dir = cfg['input_path']['data_dir']
         dataframes = []
         
-        # for file_info in config["files"]:
-        #     file_name = file_info["name"]
-        #     columns = file_info["columns"]
-        #     file_path = os.path.join(data_dir, file_name)
+        for file_info in files:
+            file_name = file_info["name"]
+            columns = file_info["columns"]
+            file_path = os.path.join(data_dir, file_name)
 
-            # if os.path.exists(file_path):
-            #     df = pd.read_csv(file_path, usecols=columns)
-            #     dataframes.append(df)
-            # else:
-            #     print(f"Warning: File not found - {file_name}")
+            if os.path.exists(file_path):
+                df = pd.read_csv(file_path, usecols=columns)
+                dataframes.append(df)
+            else:
+                print(f"Warning: File not found - {file_name}")
         
-        # merged_df = pd.concat(dataframes, axis=1)
+        merged_df = pd.concat(dataframes, axis=1)
         
-        # # merged_df.to_csv(output_path, index=False)
-        # return merged_df
+        output_path = os.path.join("tests", "modules", "analysis", "data_for_analysis", f"{label}.csv")
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)  
+        merged_df.to_csv(output_path, index=False)
 
-if __name__ == "__main__":
+        return merged_df
+
+
+if __name__ == "__main__": 
     prepare_bsee_data = PrepareBseeData()
