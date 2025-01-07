f4370900dc8d396995ea8f0ac36aa381a96c00b4
Samdan Shaik <110446718+samdansk2@users.noreply.github.com>
2025-01-07 18:51:07 +0530

    Bseedata (#19)
    consolidate repos

--- tests/legacy/test_production.py
+++ tests/legacy/test_production.py
@@ -1,5 +1,5 @@
 from energydata.common.ymlInput import ymlInput
-from energydata.common.data_models_components import DataModelsComponents
+from energydata.common.legacy.data_models_components import DataModelsComponents
 
 cfg_yaml = 'C:/GitHub/energydata/src/energydata/tests/legacy/data/bsee_yearly_production_data.yml'
 cfg = ymlInput(cfg_yaml, None)
