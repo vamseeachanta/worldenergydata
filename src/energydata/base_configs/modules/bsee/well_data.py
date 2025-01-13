0fbdc63b6744ec999cb54a29d2306b83a42265b0
samdansk2 <samdanshaik8998@gmail.com>
2025-01-13 10:53:59 +0530

    improve

--- src/energydata/base_configs/modules/bsee/well_data.py
+++ src/energydata/base_configs/modules/bsee/well_data.py
@@ -1,9 +1,10 @@
 import os
 import pandas as pd
 from energydata.custom.scrapy_for_API import BSEEDataSpider
-from energydata.custom.scrapy_for_API import run_spiders
-from energydata.custom.scrapy_for_block import run_spider
+from energydata.custom.scrapy_for_API import ScrapyRunnerAPI
+
 from energydata.custom.scrapy_for_block import BSEESpider
+from energydata.custom.scrapy_for_block import ScrapyRunner
 
 bsee_wellAPI = BSEEDataSpider()
 bsee_block = BSEESpider()
@@ -15,11 +16,19 @@ class WellData:
     
     def get_well_data(self, cfg):
         if "well_data" in cfg and cfg['well_data']['flag']:
-             input_items = cfg['input']
-             run_spiders(cfg, input_items)
+            input_items = cfg['input']
+            scrapy_runner = ScrapyRunnerAPI()
+
+            for input_item in input_items:
+                scrapy_runner.run_spider(cfg, input_item)
+            scrapy_runner.start()
         elif "block_data" in cfg and cfg['block_data']['flag']:
-             input_items = cfg['input']
-             run_spider(cfg, input_items)
+            input_items = cfg['input']
+            scrapy_runner = ScrapyRunner()
+
+            for input_item in input_items:
+                scrapy_runner.run_spider(cfg, input_item)
+            scrapy_runner.start()
 
         
 
