7653034afc92feeac7a1af6710cc64a89dd40b70
samdansk2 <samdanshaik8998@gmail.com>
2025-01-18 10:41:16 +0530

    add all yml

--- src/energydata/custom/scrapy_for_block.py
+++ src/energydata/custom/scrapy_for_block.py
@@ -86,7 +86,7 @@ class ScrapyRunner:
         self.task_count -= 1
 
         if self.task_count == 0:
-            print("All tasks completed, stopping reactor.")
+            #print("All tasks completed, stopping reactor.")
             reactor.stop()
 
     def start(self):
