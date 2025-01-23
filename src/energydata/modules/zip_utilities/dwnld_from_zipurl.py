000eab024f348fd22f35e9ba947ee127389efb34
samdansk2 <samdanshaik8998@gmail.com>
2025-01-23 11:31:35 +0530

    move main code

---
+++ src/energydata/modules/zip_utilities/dwnld_from_zipurl.py
@@ -0,0 +1,54 @@
+# Standard library imports
+import io
+import os
+import zipfile
+from urllib.parse import urlparse
+
+# Third party imports
+import requests
+import pandas as pd
+
+class DownloadFromZipUrl:
+    
+    def __init__(self):
+        pass
+
+    def router(self,cfg):
+        
+        urls = cfg['input']['urls']
+        
+        for url in range(urls):
+            self.download_and_process_zip(url ,cfg)
+
+        return cfg
+    
+    def download_and_process_zip(self, url ,cfg):
+
+        # Extract the name from the URL
+        base_name = os.path.basename(urlparse(url).path).replace('.zip', '')
+
+        r = requests.get(url)
+        r.raise_for_status()  # Check if the download was successful
+
+        z = zipfile.ZipFile(io.BytesIO(r.content)) 
+
+        extracted_files = z.namelist()
+
+        output_dir = cfg['input_data']['out_directory']
+
+        for file in extracted_files:
+            if file.endswith('/'):
+                continue
+            csv_filename = f"{base_name}_{os.path.splitext(os.path.basename(file))[0]}"+'.csv'
+            with z.open(file) as file:   
+                try:
+                    df = pd.read_csv(file, sep=',', encoding='ISO-8859-1', low_memory=False, nrows=100)
+                except Exception as e:
+                    print(f"Could not read {file} as CSV: {e}")
+                    continue
+
+                df.to_csv(os.path.join(output_dir, csv_filename), index=False)
+    
+        
+
+
