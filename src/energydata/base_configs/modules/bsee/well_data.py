867e7848cc6b867b4d7a707af1b7997d997f9825
samdansk2 <samdanshaik8998@gmail.com>
2025-01-24 11:23:33 +0530

    20252401

--- src/energydata/base_configs/modules/bsee/well_data.py
+++ src/energydata/base_configs/modules/bsee/well_data.py
@@ -1,10 +1,8 @@
-import os
-import pandas as pd
-from energydata.custom.scrapy_for_API import BSEEDataSpider
-from energydata.custom.scrapy_for_API import ScrapyRunnerAPI
+from energydata.modules.bsee.data.scrapy_for_API import BSEEDataSpider
+from energydata.modules.bsee.data.scrapy_for_API import ScrapyRunnerAPI
 
-from energydata.custom.scrapy_for_block import BSEESpider
-from energydata.custom.scrapy_for_block import ScrapyRunner
+from energydata.modules.bsee.data.scrapy_for_block import BSEESpider
+from energydata.modules.bsee.data.scrapy_for_block import ScrapyRunner
 
 bsee_wellAPI = BSEEDataSpider()
 bsee_block = BSEESpider()
