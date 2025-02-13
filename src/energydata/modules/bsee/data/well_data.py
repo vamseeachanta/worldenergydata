111a0a615e1ed1849d2a146571399f6959312167
samdansk2 <samdanshaik8998@gmail.com>
2025-02-13 12:46:49 +0530

    by lease wip

--- src/energydata/modules/bsee/data/well_data.py
+++ src/energydata/modules/bsee/data/well_data.py
@@ -1,7 +1,8 @@
 import os
 from copy import deepcopy
-from energydata.modules.bsee.analysis.scrapy_for_block import ScrapyRunner
+from energydata.modules.bsee.analysis.scrapy_for_block import ScrapyRunnerBlock
 from energydata.modules.bsee.data.scrapy_for_API import  ScrapyRunnerAPI
+from energydata.modules.bsee.data.scrapy_production_data import ScrapyRunnerProduction
 
 
 class WellData:
@@ -18,26 +19,36 @@ class WellData:
         output_data = [] 
         if "well_data" in cfg and cfg['well_data']['flag']:
             input_items = cfg['settings']
-            scrapy_runner = ScrapyRunnerAPI()
+            scrapy_runner_api = ScrapyRunnerAPI()
 
             for input_item in input_items:
-                scrapy_runner.run_spider(cfg, input_item)
+                scrapy_runner_api.run_spider(cfg, input_item)
 
                 #output_data = self.generate_output_item(cfg, output_data, input_item)
 
-            scrapy_runner.start()
+            scrapy_runner_api.start()
+        
+        elif "production" in cfg and cfg['production']['flag']:
+            input_items = cfg['settings']
+            scrapy_runner_production = ScrapyRunnerProduction()
+
+            for input_item in input_items:
+                scrapy_runner_production.run_spider(cfg, input_item)
+                output_data = self.generate_output_item(output_data, input_item)
+                
+            scrapy_runner_production.start()
 
         elif "block_data" in cfg and cfg['block_data']['flag'] or "well_production" in cfg and cfg['well_production']['flag']:
             input_items = cfg['settings']
-            scrapy_runner = ScrapyRunner()
+            scrapy_runner_block = ScrapyRunnerBlock()
 
             for input_item in input_items:
                 # well_data_scrapper_cfg = deepcopy(input_item.copy())
                 # well_data_scrapper_cfg.update({'output_dir': output_path})
-                scrapy_runner.run_spider(cfg, input_item)
+                scrapy_runner_block.run_spider(cfg, input_item)
                 output_data = self.generate_output_item(output_data, input_item)
                 
-            scrapy_runner.start()
+            scrapy_runner_block.start()
 
         well_data = {'type': 'csv', 'groups': output_data}
         cfg[cfg['basename']].update({'well_data': well_data})
