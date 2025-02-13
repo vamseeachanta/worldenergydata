7c94c34948ea6079c124cdfb595ef2566a36f6a7
Vamsee Achanta <achantav@gmail.com>
2025-02-13 09:46:41 -0600

    fix well data path.

--- src/energydata/modules/bsee/data/well_data.py
+++ src/energydata/modules/bsee/data/well_data.py
@@ -4,6 +4,8 @@ from energydata.modules.bsee.analysis.scrapy_for_block import ScrapyRunnerBlock
 from energydata.modules.bsee.data.scrapy_for_API import  ScrapyRunnerAPI
 from energydata.modules.bsee.data.scrapy_production_data import ScrapyRunnerProduction
 
+from assetutilities.common.utilities import is_dir_valid_func
+
 
 class WellData:
     
@@ -34,7 +36,7 @@ class WellData:
 
             for input_item in input_items:
                 scrapy_runner_production.run_spider(cfg, input_item)
-                output_data = self.generate_output_item(output_data, input_item)
+                output_data = self.generate_output_item(cfg, output_data, input_item)
                 
             scrapy_runner_production.start()
 
@@ -46,7 +48,7 @@ class WellData:
                 # well_data_scrapper_cfg = deepcopy(input_item.copy())
                 # well_data_scrapper_cfg.update({'output_dir': output_path})
                 scrapy_runner_block.run_spider(cfg, input_item)
-                output_data = self.generate_output_item(output_data, input_item)
+                output_data = self.generate_output_item(cfg, output_data, input_item)
                 
             scrapy_runner_block.start()
 
@@ -55,9 +57,16 @@ class WellData:
         
         return cfg
 
-    def generate_output_item(self, output_data, input_item):
+    def generate_output_item(self, cfg, output_data, input_item):
         label = input_item['label']
         output_path = input_item['output_dir']
+        if output_path is None:
+            result_folder = cfg['Analysis']['result_folder']
+            output_path = os.path.join(result_folder, 'Data')
+
+        analysis_root_folder = cfg['Analysis']['analysis_root_folder']
+        is_dir_valid, output_path = is_dir_valid_func(output_path, analysis_root_folder)
+
         file_path = os.path.join(output_path, f"{label}.csv")
         input_item_csv_cfg = deepcopy(input_item)
         input_item_csv_cfg.update({'label': label, 'file_name': file_path})
