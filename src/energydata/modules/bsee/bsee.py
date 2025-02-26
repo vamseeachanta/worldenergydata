862a42f5f6e223dadd215f6ef73dced409fb9d67
Vamsee Achanta <achantav@gmail.com>
2025-02-26 14:25:41 -0600

    20250226

--- src/energydata/modules/bsee/bsee.py
+++ src/energydata/modules/bsee/bsee.py
@@ -25,27 +25,17 @@ class bsee:
 
     def router(self, cfg):
         # Update configuration with master data
-        # cfg = self.get_cfg_with_master_data(cfg)
+        cfg = self.get_cfg_with_master_data(cfg)
         
         cfg[cfg['basename']] = {}
-        # Route to appropriate data processing based on configuration flags
         if 'well_data' in cfg and cfg['well_data']['flag'] or 'block_data' in cfg and cfg['block_data']['flag']:
-            cfg  = well_data.get_well_data_all_wells(cfg)
+            cfg, well_data_groups  = well_data.get_well_data_all_wells(cfg)
         
         if 'production' in cfg and cfg['production']['flag'] or 'well_production' in cfg and cfg['well_production']['flag']:
             cfg = production_data.get_well_data(cfg)
 
-        if 'well_prod_data' in cfg and cfg['well_prod_data']['flag']:
-            gwp.router(cfg)
-
-        if 'existing_data' in cfg and cfg['existing_data']['flag']:
-            cfg = gdff.router(cfg)
-
         if 'analysis' in cfg and cfg['analysis']['flag']:
-            bsee_analysis.router(cfg)
-        
-        elif "data_prep" in cfg and cfg["data_prep"]["flag"]:
-            data = prep_bsee_data.router(cfg)
+            cfg = bsee_analysis.router(cfg, well_data_groups)
 
         return cfg
 
@@ -66,5 +56,6 @@ class bsee:
         #         group = update_deep_dictionary(settings_master, group)
         #         items[item_idx] = group.copy()
 
-        # return cfg
         pass
+    
+        return cfg
