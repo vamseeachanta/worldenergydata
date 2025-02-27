5705fb2772203402d60878949e6189f10cba4cc7
samdansk2 <samdanshaik8998@gmail.com>
2025-02-27 19:39:57 +0530

    get block data

--- src/energydata/modules/bsee/bsee.py
+++ src/energydata/modules/bsee/bsee.py
@@ -4,13 +4,15 @@ from assetutilities.common.update_deep import update_deep_dictionary #noqa
 from energydata.modules.bsee.data.production_data_from_zip import GetWellProdDataFromZip
 #from energydata.modules.bsee.data.production_data_from_website import GetWellProdDataFromWebsite
 from energydata.modules.bsee.data.well_data import WellData
-from energydata.modules.bsee.analysis.production_data import ProductionDataWebsite
+from energydata.modules.bsee.data.production_data import ProductionDataWebsite
+from energydata.modules.bsee.data.block_data import BlockDataWebsite
 from energydata.modules.bsee.analysis.prepare_data_for_analysis import PrepareBseeData
 from energydata.modules.bsee.data.scrapy_production_data import SpiderBsee
 from energydata.modules.bsee.analysis.bsee_analysis import BSEEAnalysis
 
 # Initialize instances of imported classes
 production_from_website = ProductionDataWebsite()
+block_data_website = BlockDataWebsite()
 bsee_production = SpiderBsee()
 well_data = WellData()
 #production_data = GetWellProdDataFromWebsite()
@@ -28,12 +30,15 @@ class bsee:
         cfg = self.get_cfg_with_master_data(cfg)
         
         cfg[cfg['basename']] = {}
-        if 'well_data' in cfg and cfg['well_data']['flag'] or 'block_data' in cfg and cfg['block_data']['flag']:
+        if 'well_data' in cfg and cfg['well_data']['flag'] :
             cfg, well_data_groups  = well_data.get_well_data_all_wells(cfg)
         
         if 'production' in cfg and cfg['production']['flag'] or \
           'well_production' in cfg and cfg['well_production']['flag']:
             cfg, well_data_groups = production_from_website.get_data(cfg)
+        
+        elif 'block_data' in cfg and cfg['block_data']['flag']:
+            cfg, well_data_groups = block_data_website.get_data(cfg)
 
         if 'analysis' in cfg and cfg['analysis']['flag']:
             cfg = bsee_analysis.router(cfg, well_data_groups)
