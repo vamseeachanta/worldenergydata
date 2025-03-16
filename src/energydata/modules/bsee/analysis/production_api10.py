e632ebdb30b90985251a67cb59893716522ee49c
Vamsee Achanta <achantav@gmail.com>
2025-03-16 16:28:29 -0500

    20250316

---
+++ src/energydata/modules/bsee/analysis/production_api10.py
@@ -0,0 +1,89 @@
+# Standard library imports
+import os
+import json
+import logging
+
+# # # Third party imports
+import pandas as pd
+from energydata.modules.bsee.data.bsee_data import BSEEData
+from energydata.common.legacy.data import DateTimeUtility
+
+# from energydata.common.bsee_data_manager import BSEEData
+
+# from energydata.common.data import AttributeDict, transform_df_datetime_to_str
+
+bsee_data = BSEEData()
+dtu = DateTimeUtility()
+
+class ProductionAnalysis():
+
+    def __init__(self):
+        pass
+
+    def router(self, cfg, api12_production_data):
+        self.prepare_production_data(cfg, api12_production_data)
+
+        self.prepare_field_production_rate(df_temp, completion_name)
+        self.prepare_field_production(df_temp, completion_name)
+
+
+    def add_production_from_all_wells(self):
+        # Third party imports
+        import pandas as pd
+        columns = self.output_data_field_production_rate_df.columns.tolist()
+        columns.remove('PRODUCTION_DATETIME')
+        self.output_data_field_production_rate_df[
+            'TOTAL_DAILY_PRODUCTION_rate_BOPD'] = self.output_data_field_production_rate_df[columns].sum(axis=1)
+
+        columns = self.output_data_field_production_df.columns.tolist()
+        columns.remove('PRODUCTION_DATETIME')
+        self.output_data_field_production_df[
+            'Total_MONTLY_PRODUCTION_MMbbl'] = self.output_data_field_production_df[columns].sum(axis=1) / 1000 / 1000
+        self.output_data_field_production_df[
+            'CUMULATIVE_MONTLY_PRODUCTION_MMbbl'] = self.output_data_field_production_df[
+                'Total_MONTLY_PRODUCTION_MMbbl'].cumsum()
+
+        self.field_summary['Cummulative Production, MMbbls'] = {
+            'PRODUCTION_DATETIME':
+                self.output_data_field_production_df['PRODUCTION_DATETIME'].tolist(),
+            'CUMULATIVE_MONTLY_PRODUCTION_MMbbl':
+                self.output_data_field_production_df['CUMULATIVE_MONTLY_PRODUCTION_MMbbl'].tolist()
+        }
+
+        self.production_summary_df = pd.DataFrame()
+        self.production_summary_df['PRODUCTION_DATETIME'] = self.output_data_field_production_rate_df[
+            'PRODUCTION_DATETIME']
+        self.production_summary_df['Field NickName'] = self.cfg['custom_parameters']['field_nickname']
+        self.production_summary_df['BOEM_FIELDS'] = self.cfg['custom_parameters']['boem_fields']
+        self.production_summary_df['Production Rate, BOPD'] = self.output_data_field_production_rate_df[
+            'TOTAL_DAILY_PRODUCTION_rate_BOPD']
+        self.production_summary_df['CUMULATIVE_MONTLY_PRODUCTION_MMbbl'] = self.output_data_field_production_df[
+            'CUMULATIVE_MONTLY_PRODUCTION_MMbbl']
+
+
+    def prepare_field_production_rate(self, df_temp, df_column_label):
+        self.output_data_field_production_rate_df = pd.DataFrame(columns=['PRODUCTION_DATETIME'])
+
+        field_production_rate_df = pd.DataFrame()
+        field_production_rate_df['PRODUCTION_DATETIME'] = df_temp['PRODUCTION_DATETIME'].copy()
+        field_production_rate_df[df_column_label] = df_temp['O_PROD_RATE_BOPD'].copy()
+        self.output_data_field_production_rate_df = pd.merge(left=self.output_data_field_production_rate_df,
+                                                                right=field_production_rate_df,
+                                                                how='outer',
+                                                                left_on='PRODUCTION_DATETIME',
+                                                                right_on='PRODUCTION_DATETIME')
+        self.output_data_field_production_rate_df.sort_values(by=['PRODUCTION_DATETIME'], inplace=True)
+
+    def prepare_field_production(self, df_temp, df_column_label):
+        self.output_data_field_production_df = pd.DataFrame(columns=['PRODUCTION_DATETIME'])
+
+        field_production_df = pd.DataFrame()
+        field_production_df['PRODUCTION_DATETIME'] = df_temp['PRODUCTION_DATETIME'].copy()
+        field_production_df[df_column_label] = df_temp['MON_O_PROD_VOL'].copy()
+        self.output_data_field_production_df = pd.merge(left=self.output_data_field_production_df,
+                                                        right=field_production_df,
+                                                        how='outer',
+                                                        left_on='PRODUCTION_DATETIME',
+                                                        right_on='PRODUCTION_DATETIME')
+        self.output_data_field_production_df.sort_values(by=['PRODUCTION_DATETIME'], inplace=True)
+        self.output_data_field_production_df.drop_duplicates(inplace=True)
