6f7214bb37bc496e383da5162d615ba9def67d41
Vamsee Achanta <achantav@gmail.com>
2025-04-01 11:52:44 -0500

    20250401

--- src/energydata/engine.py
+++ src/energydata/engine.py
@@ -10,7 +10,6 @@ from assetutilities.common.data import (
     SaveData,
 )
 from assetutilities.common.file_management import FileManagement
-from assetutilities.common.yml_utilities import ymlInput  #noqa
 
 from assetutilities.common.ApplicationManager import ConfigureApplicationInputs
 from assetutilities.common.yml_utilities import WorkingWithYAML
@@ -20,36 +19,39 @@ from energydata.modules.bsee.bsee import bsee
 #from energydata.modules.bsee.data.bsee_data import BSEEData
 from energydata.modules.bsee.zip_data_dwnld.zip import zip
 
+app_manager = ConfigureApplicationInputs()
 save_data = SaveData()
-wwy = WorkingWithYAML()
-
-#bsee_data = BSEEData()
+wwyaml = WorkingWithYAML()
 
 library_name = "energydata"
 
 
 def engine(inputfile: str = None, cfg: dict = None, config_flag: bool = True) -> dict:
     
+    cfg_argv_dict = {}
     if cfg is None:
-        inputfile = validate_arguments_run_methods(inputfile)
-        cfg = wwy.ymlInput(inputfile, updateYml=None)
+        inputfile, cfg_argv_dict = app_manager.validate_arguments_run_methods(inputfile)
+        cfg = wwyaml.ymlInput(inputfile, updateYml=None)
         cfg = AttributeDict(cfg)
         if cfg is None:
             raise ValueError("cfg is None")
 
-    basename = cfg["basename"]
-    application_manager = ConfigureApplicationInputs(basename)
-    application_manager.configure(cfg, library_name)
+    if 'basename' in cfg:
+        basename = cfg["basename"]
+    elif 'meta' in cfg:
+        basename = cfg["meta"]["basename"]
+    else:
+        raise ValueError("basename not found in cfg")
 
     if config_flag:
         fm = FileManagement()
-        cfg_base = application_manager.cfg
+        cfg_base = app_manager.configure(cfg, library_name, basename, cfg_argv_dict)
         cfg_base = fm.router(cfg_base)
+        result_folder_dict, cfg_base = app_manager.configure_result_folder(None, cfg_base)
     else:
         cfg_base = cfg
 
     logger.info(f"{basename}, application ... START")
-
 
     if basename in ["bsee"]:
         bsee_app = bsee()
@@ -63,44 +65,7 @@ def engine(inputfile: str = None, cfg: dict = None, config_flag: bool = True) ->
         raise (Exception(f"Analysis for basename: {basename} not found. ... FAIL"))
 
     logger.info(f"{basename}, application ... END")
-    save_cfg(cfg_base=cfg_base)
+    app_manager.save_cfg(cfg_base=cfg_base)
 
     return cfg_base
 
-
-def validate_arguments_run_methods(inputfile):
-    """
-    Validate inputs for following run methods:
-    - module (i.e. python -m digitalmodel input.yml)
-    - from python file (i.e. )
-    """
-
-    if len(sys.argv) > 1 and inputfile is not None:
-        raise (
-            Exception(
-                "2 Input files provided via arguments & function. Please provide only 1 file ... FAIL"
-            )
-        )
-
-    if len(sys.argv) > 1:
-        if not os.path.isfile(sys.argv[1]):
-            raise (FileNotFoundError(f"Input file {sys.argv[1]} not found ... FAIL"))
-        else:
-            inputfile = sys.argv[1]
-
-    if len(sys.argv) <= 1:
-        if not os.path.isfile(inputfile):
-            raise (FileNotFoundError(f"Input file {inputfile} not found ... FAIL"))
-        else:
-            sys.argv.append(inputfile)
-
-    return inputfile
-
-
-def save_cfg(cfg_base):
-    output_dir = cfg_base.Analysis["analysis_root_folder"]
-
-    filename = cfg_base.Analysis["file_name"]
-    filename_path = os.path.join(output_dir, 'results', filename)
-
-    save_data.saveDataYaml(cfg_base, filename_path, default_flow_style=False)
