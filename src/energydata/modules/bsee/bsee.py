bc6e78ea8032afc98e147e667891694db6add46f
samdansk2 <samdanshaik8998@gmail.com>
2025-04-14 12:38:13 +0530

    wip

--- src/energydata/modules/bsee/bsee.py
+++ src/energydata/modules/bsee/bsee.py
@@ -1,7 +1,5 @@
 from energydata.modules.bsee.data.bsee_data import BSEEData
 from energydata.modules.bsee.analysis.bsee_analysis import BSEEAnalysis
-from energydata.modules.bsee.data.production_data_from_zip import GetProdDataFromZip
-prod_data = GetProdDataFromZip()
 
 bsee_data = BSEEData()
 bsee_analysis = BSEEAnalysis()
@@ -12,10 +10,10 @@ class bsee:
         pass
 
     def router(self, cfg):
-        prod_data.router(cfg)
-        # cfg[cfg['basename']] = {}
-        # cfg[cfg['basename']].update({'data': cfg['data']})
-        # cfg[cfg['basename']].update({'analysis': cfg['analysis']})
+    
+        cfg[cfg['basename']] = {}
+        cfg[cfg['basename']].update({'data': cfg['data']})
+        cfg[cfg['basename']].update({'analysis': cfg['analysis']})
 
         cfg, data = bsee_data.router(cfg)
 
