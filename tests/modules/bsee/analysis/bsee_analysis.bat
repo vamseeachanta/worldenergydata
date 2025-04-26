3cf6113b35fa12117c8b0cd0a5416a5b2a776b18
Vamsee Achanta <achantav@gmail.com>
2025-04-26 08:47:57 -0500

    20250426

--- tests/modules/bsee/analysis/bsee_analysis.bat
+++ tests/modules/bsee/analysis/bsee_analysis.bat
@@ -7,6 +7,7 @@ REM python -m energydata query_api_01_block_api12.yml
 
 python -m energydata query_field_julia.yml
 python -m energydata query_field_stmalo.yml
+python -m energydata query_field_stones.yml
 
 @REM Run individual field blocks
 @REM python -m energydata query_field_stmalo.yml "{'meta': {'label': 'goa_stmalo_633'}, 'data': {'groups': [{'bottom_block': [633]}]}}"
@@ -17,3 +18,6 @@ python -m energydata query_field_stmalo.yml
 @REM python -m energydata query_field_julia.yml "{'meta': {'label': 'goa_julia_678'}, 'data': {'groups': [{'bottom_block': [678]}]}}"
 @REM python -m energydata query_field_julia.yml "{'meta': {'label': 'goa_julia_758'}, 'data': {'groups': [{'bottom_block': [758]}]}}"
 @REM python -m energydata query_field_julia.yml "{'meta': {'label': 'goa_julia_759'}, 'data': {'groups': [{'bottom_block': [759]}]}}"
+
+@REM python -m energydata query_field_stones.yml "{'meta': {'label': 'goa_julia_508'}, 'data': {'groups': [{'bottom_block': [508]}]}}"
+@REM python -m energydata query_field_stones.yml "{'meta': {'label': 'goa_julia_464'}, 'data': {'groups': [{'bottom_block': [509]}]}}"
