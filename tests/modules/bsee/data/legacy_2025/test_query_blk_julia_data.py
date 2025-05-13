4ed35cb80f1c1b5f51d778ea7a3e1a57d214800f
samdansk2 <samdanshaik8998@gmail.com>
2025-05-13 11:44:59 +0530

    check tests run

--- tests/modules/bsee/data/legacy_2025/test_query_blk_julia_data.py
+++ tests/modules/bsee/data/legacy_2025/test_query_blk_julia_data.py
@@ -23,11 +23,7 @@ def get_valid_pytest_output_file(pytest_output_file):
 
 
 def test_application():
-<<<<<<< HEAD:tests/modules/bsee/data/test_query_blk_julia_API.py
-    input_file = 'query_blk_julia_API.yml'
-=======
     input_file = 'query_blk_julia_data.yml'
->>>>>>> 202502:tests/modules/bsee/data/legacy_2025/test_query_blk_julia_data.py
     pytest_output_file = None
     # pytest_output_file = get_valid_pytest_output_file(pytest_output_file)
     # expected_result = ymlInput(pytest_output_file, updateYml=None)
