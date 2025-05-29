8045662913ed6ca5d419829038bbaee250924482
samdansk2 <samdanshaik8998@gmail.com>
2025-05-29 12:00:05 +0530

    fix well_summary_julia

--- src/worldenergydata/modules/bsee/data/by_block.py
+++ src/worldenergydata/modules/bsee/data/by_block.py
@@ -1,8 +1,11 @@
 import logging
 import pandas as pd
 from worldenergydata.modules.bsee.data._by_block.data_from_url import DataFromURL
+from worldenergydata.modules.bsee.data._by_block.data_from_bin import DataFromBin
 
-block_data = DataFromURL()
+
+block_data_from_url = DataFromURL()
+block_data_from_bin = DataFromBin()
 
 class Block:
        
@@ -24,9 +27,13 @@ class Block:
 
         return cfg
     
-    def add_api12_array_by_block_to_cfg(self, cfg):
-        cfg, block_data_groups = block_data.router(cfg)
-        
+    def add_api12_array_to_cfg(self, cfg):
+
+        if 'from_bin' in cfg['data'] and cfg['data']['from_bin']:
+            cfg, block_data_groups = block_data_from_bin.router(cfg)
+        else:
+            cfg, block_data_groups = block_data_from_url.router(cfg)
+
         return cfg
     
 
