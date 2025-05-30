cb70e12141e9cc074ed10e806fa56d726a3493fa
Vamsee Achanta <achantav@gmail.com>
2025-05-30 11:26:57 -0500

    wip

--- src/worldenergydata/modules/bsee/data/by_block.py
+++ src/worldenergydata/modules/bsee/data/by_block.py
@@ -14,16 +14,8 @@ class Block:
     
     def router(self, cfg):
 
-        #TODO relocate as necessary
-        # groups = cfg['data']['groups']
-        # for group in groups:
-        #     if 'bottom_block' in group and group['bottom_block'] is not None:
-        #         group_block = group['bottom_block']
-        #         group_api12 = group['api12']
-        #         if group_api12 is not None:
-        #             logging.warning('group item running by Block data. API12 input NOT used')
-        if 'analysis' in cfg and cfg['analysis']['bottom_blocks']:
-            cfg = self.add_api12_array_by_block_to_cfg(cfg)
+        if cfg['data']['groups'][0]['bottom_block'] is not None:
+            cfg = self.add_api12_array_to_cfg(cfg)
 
         return cfg
     
