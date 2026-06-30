3feff18f136ad67f8380984c7f181bae326eba75
Vamsee Achanta <achantav@gmail.com>
2026-06-30 07:09:50 -0500

    feat: add texas rrc lifecycle api keys

---
+++ packages/worldenergydata-texas_rrc/src/worldenergydata/texas_rrc/lifecycle/__init__.py
@@ -0,0 +1,9 @@
+"""Lifecycle normalization tools for Texas RRC well data."""
+
+from worldenergydata.texas_rrc.lifecycle.keys import (
+    derive_api10,
+    normalize_api14,
+    split_api14,
+)
+
+__all__ = ["derive_api10", "normalize_api14", "split_api14"]
